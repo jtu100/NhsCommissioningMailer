2cedf34efade12110bf09da88fa9548cc6111257
Jack Ukleja <jack@ukleja.com>
2013-01-26 14:27:39 +0000

    .Net 4 x86, ignore packages

--- CommissioningMailer/Class1.cs
+++ CommissioningMailer/Class1.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LinqToExcel;
+using NUnit.Framework;
 
 namespace CommissioningMailer
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void CanLoadAllSurgeries()
+        {
+            var surgeries = new SurgeriesRepo("SampleData\\Surgeries.csv").GetAll();
+            foreach (var surgery in surgeries)
+            {
+                Console.WriteLine(surgery.PracticeCode);
+            }
+        }
+    }
+
+    public class SurgeriesRepo
+    {
+        private readonly string _csvFilePath;
+
+        public SurgeriesRepo(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public IEnumerable<Surgery> GetAll()
+        {
+            var excel = new ExcelQueryFactory(_csvFilePath);
+            var surgeries = from s in excel.Worksheet<Surgery>()
+                                  select s;
+            return surgeries;
+        }
+    }
+
+    public class Surgery
+    {
+        public string PracticeCode { get; set; }
+        public string LeadGp { get; set; }
+        public string PracticeName { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
